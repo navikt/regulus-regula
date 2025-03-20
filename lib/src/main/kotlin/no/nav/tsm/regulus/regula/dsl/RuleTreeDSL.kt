3ec2c8484689993f9b7bd4998491154ad25b40af
Karl J. Overå <k@rl.run>
2025-03-20 07:59:14 +0100

    chore: more explicit generic names

--- lib/src/main/kotlin/no/nav/tsm/regulus/regula/dsl/RuleTreeDSL.kt
+++ lib/src/main/kotlin/no/nav/tsm/regulus/regula/dsl/RuleTreeDSL.kt
@@ -1,35 +1,32 @@
 package no.nav.tsm.regulus.regula.dsl
 
-sealed class TreeNode<T, R>
+sealed class TreeNode<Enum, Result>
 
-class ResultNode<T, R>(val result: R) : TreeNode<T, R>()
+class ResultNode<Enum, Result>(val result: Result) : TreeNode<Enum, Result>()
 
-class RuleNode<T, R> internal constructor(val rule: T) : TreeNode<T, R>() {
-    lateinit var yes: TreeNode<T, R>
-    lateinit var no: TreeNode<T, R>
+class RuleNode<Enum, Result> internal constructor(val rule: Enum) : TreeNode<Enum, Result>() {
+    lateinit var yes: TreeNode<Enum, Result>
+    lateinit var no: TreeNode<Enum, Result>
 
-    internal fun yes(rule: T, init: RuleNode<T, R>.() -> Unit) {
-        yes = RuleNode<T, R>(rule).apply(init)
+    internal fun yes(rule: Enum, init: RuleNode<Enum, Result>.() -> Unit) {
+        yes = RuleNode<Enum, Result>(rule).apply(init)
     }
 
-    internal fun no(rule: T, init: RuleNode<T, R>.() -> Unit) {
-        no = RuleNode<T, R>(rule).apply(init)
+    internal fun no(rule: Enum, init: RuleNode<Enum, Result>.() -> Unit) {
+        no = RuleNode<Enum, Result>(rule).apply(init)
     }
 
-    internal fun yes(result: R) {
+    internal fun yes(result: Result) {
         yes = ResultNode(result)
     }
 
-    internal fun no(result: R) {
+    internal fun no(result: Result) {
         no = ResultNode(result)
     }
 }
 
-fun <T, R> tree(rule: T, init: RuleNode<T, R>.() -> Unit): RuleNode<T, R> =
-    RuleNode<T, R>(rule).apply(init)
+fun <Enum, Result> tree(rule: Enum, init: RuleNode<Enum, Result>.() -> Unit): RuleNode<Enum, Result> =
+    RuleNode<Enum, Result>(rule).apply(init)
 
-fun <T, R> rule(rule: T, init: RuleNode<T, R>.() -> Unit): RuleNode<T, R> =
-    RuleNode<T, R>(rule).apply(init)
-
-fun <T, R> subTree(rule: T, init: RuleNode<T, R>.() -> Unit): RuleNode<T, R> =
-    RuleNode<T, R>(rule).apply(init)
+fun <Enum, Result> rule(rule: Enum, init: RuleNode<Enum, Result>.() -> Unit): RuleNode<Enum, Result> =
+    RuleNode<Enum, Result>(rule).apply(init)
--- lib/src/main/kotlin/no/nav/tsm/regulus/regula/dsl/Rules.kt
+++ lib/src/main/kotlin/no/nav/tsm/regulus/regula/dsl/Rules.kt
@@ -1,23 +1,23 @@
 package no.nav.tsm.regulus.regula.dsl
 
-data class RuleOutput<T>(
+data class RuleOutput<Enum>(
     val ruleInputs: Map<String, Any> = emptyMap(),
     val ruleResult: Boolean,
-    val rule: T,
+    val rule: Enum,
 )
 
-data class TreeOutput<T, S>(
+data class TreeOutput<Enum, Result>(
     val ruleInputs: Map<String, Any> = mapOf(),
-    val rulePath: List<RuleOutput<T>> = emptyList(),
-    val treeResult: S,
+    val rulePath: List<RuleOutput<Enum>> = emptyList(),
+    val treeResult: Result,
 )
 
-fun <T, S> TreeOutput<T, S>.printRulePath(): String =
+fun <Enum, Result> TreeOutput<Enum, Result>.printRulePath(): String =
     rulePath
         .joinToString(separator = "->") { "${it.rule}(${if (it.ruleResult) "yes" else "no"})" }
         .plus("->$treeResult")
 
-infix fun <T, S> RuleOutput<T>.join(rulesOutput: TreeOutput<T, S>) =
+infix fun <Enum, Result> RuleOutput<Enum>.join(rulesOutput: TreeOutput<Enum, Result>) =
     TreeOutput(
         ruleInputs = ruleInputs + rulesOutput.ruleInputs,
         rulePath = listOf(this) + rulesOutput.rulePath,
