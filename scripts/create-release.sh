e2ccbad04f15a05f7d90a938951c2f4cb4629511
Karl J. Overå <k@rl.run>
2025-03-26 12:31:09 +0100

    chore: gotta commit new version back

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Commit bumped version
+git config user.name "github-actions[bot]"
+git config user.email "github-actions[bot]@users.noreply.github.com"
+git add lib/version
+git commit -m "Set next version to $VERSION [skip ci]"
+git push origin HEAD
+
 VERSION=$(cat lib/version)
 LAST_TAG=$(git tag --sort=-creatordate | head -n 1)
 COMMITS=$(git log ${LAST_TAG}..HEAD --pretty=format:"* %s")
