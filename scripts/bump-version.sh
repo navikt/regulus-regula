9edd0df49311b944d4f4e553e3fbe5234f4f1f57
Karl J. Overå <k@rl.run>
2025-04-02 13:19:23 +0200

    chore: tweak release commit msg [skip ci]
    skip release

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -10,5 +10,5 @@ echo $((VERSION + 1)) > lib/version
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 git add lib/version
-git commit -m "chore: $VERSION released! set next version to $((VERSION + 1)) [skip ci]"
+git commit -m "chore: v$VERSION released! set next version to v$((VERSION + 1)) [skip ci]"
 git push origin HEAD
