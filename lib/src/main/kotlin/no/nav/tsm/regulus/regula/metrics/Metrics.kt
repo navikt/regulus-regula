7617916d721134c70e5d93d6e970fbc5ef7e1133
Karl J. Overå <k@rl.run>
2025-04-02 13:25:59 +0200

    chore: remove metrics log statement

--- lib/src/main/kotlin/no/nav/tsm/regulus/regula/metrics/Metrics.kt
+++ lib/src/main/kotlin/no/nav/tsm/regulus/regula/metrics/Metrics.kt
@@ -1,15 +1,7 @@
 package no.nav.tsm.regulus.regula.metrics
 
-import org.slf4j.LoggerFactory
-
 private val clientId = System.getenv("NAIS_CLIENT_ID") ?: "unknown"
 
-private val logger = LoggerFactory.getLogger("regula.metrics")
-
 internal fun registerVersionMetrics() {
-    logger.info(
-        "Registering version metrics for clientId: $clientId, version: ${RegulaVersion.VERSION}"
-    )
-
     libVersionMetric.labels(clientId).set(RegulaVersion.VERSION.toDouble())
 }
