8cb05d6eb17e7bf00a93c9b47732bad6fd191d29
Karl J. Overå <k@rl.run>
2025-04-04 14:54:39 +0200

    chore: add test for MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39
    skip release

--- lib/src/test/kotlin/no/nav/tsm/regulus/regula/rules/trees/validering/ValideringRulesTest.kt
+++ lib/src/test/kotlin/no/nav/tsm/regulus/regula/rules/trees/validering/ValideringRulesTest.kt
@@ -153,6 +153,128 @@ class ValideringRulesTest {
     }
 
     @Test
+    fun `Manglende en av de dynamiske sporsmalene for ruleset v2 uke 39, INVALID`() {
+        val perioderMedFomForDritlengesiden =
+            listOf(Aktivitet.IkkeMulig(fom = LocalDate.now().minusDays(274), tom = LocalDate.now()))
+        val utdypendeOpplysninger =
+            mapOf(
+                "6.5" to
+                    mapOf(
+                        "6.5.1" to
+                            mapOf(
+                                "svar" to
+                                    "Thomson designs troy ratings differently york arrived, leaders scared done stanford assess package vegetarian, birmingham son preparation take forgot over hearings, being. "
+                            ),
+                        "6.5.3" to
+                            mapOf(
+                                "svar" to
+                                    "Outline communist rear charming therapist grab pendant, reliability flesh acquired champagne typing keep strip, conjunction compact stuffed witness nova invisible setup. "
+                            ),
+                        "6.5.4" to
+                            mapOf(
+                                "svar" to
+                                    "Font trains vaccine assistant nano feedback inexpensive, web charles malpractice field dodge statewide redhead, velocity movies shot converter biz journey filename, occasion aerospace holland tank. "
+                            ),
+                    )
+            )
+
+        val result =
+            ValideringRules(
+                    ValideringRulePayload(
+                        sykmeldingId = "sykmeldingId",
+                        rulesetVersion = "2",
+                        aktivitet = perioderMedFomForDritlengesiden,
+                        legekontorOrgnr = null,
+                        behandlerFnr = "07091912345",
+                        pasientIdent = "08201023912",
+                        avsenderFnr = "08201023913",
+                        utdypendeOpplysninger = utdypendeOpplysninger,
+                        papirsykmelding = false,
+                    )
+                )
+                .execute(ExecutionMode.NORMAL)
+
+        assertEquals(result.treeResult.status, RuleStatus.INVALID)
+        assertPath(
+            result.rulePath,
+            listOf(
+                ValideringRule.UGYLDIG_ORGNR_LENGDE to false,
+                ValideringRule.PAPIRSYKMELDING to false,
+                ValideringRule.UGYLDIG_REGELSETTVERSJON to false,
+                ValideringRule.MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39 to true,
+            ),
+        )
+
+        assertEquals(
+            result.ruleInputs - "utdypendeOpplysninger",
+            mapOf(
+                "rulesetVersion" to "2",
+                "papirsykmelding" to false,
+                "sykmeldingPerioder" to perioderMedFomForDritlengesiden,
+                "legekontorOrgnr" to "",
+            ),
+        )
+
+        assertEquals(
+            result.treeResult.getOutcome(),
+            ValideringRule.Outcomes.MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39,
+        )
+    }
+
+    @Test
+    fun `Behandler samme som pasient, Status INVALID`() {
+        val pasientFnr = generatePersonNumber(LocalDate.now().minusYears(31), false)
+
+        val result =
+            ValideringRules(
+                    ValideringRulePayload(
+                        sykmeldingId = "sykmeldingId",
+                        rulesetVersion = "2",
+                        aktivitet = emptyList(),
+                        legekontorOrgnr = null,
+                        behandlerFnr = pasientFnr,
+                        pasientIdent = pasientFnr,
+                        avsenderFnr = "08201023912",
+                        utdypendeOpplysninger = emptyMap(),
+                        papirsykmelding = false,
+                    )
+                )
+                .execute(ExecutionMode.NORMAL)
+
+        assertEquals(result.treeResult.status, RuleStatus.INVALID)
+        assertPath(
+            result.rulePath,
+            listOf(
+                ValideringRule.UGYLDIG_ORGNR_LENGDE to false,
+                ValideringRule.PAPIRSYKMELDING to false,
+                ValideringRule.UGYLDIG_REGELSETTVERSJON to false,
+                ValideringRule.MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39 to false,
+                ValideringRule.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR to false,
+                ValideringRule.BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR to true,
+            ),
+        )
+
+        assertEquals(
+            result.ruleInputs,
+            mapOf(
+                "rulesetVersion" to "2",
+                "papirsykmelding" to false,
+                "sykmeldingPerioder" to emptyList<Any>(),
+                "utdypendeOpplysninger" to emptyMap<String, Any>(),
+                "legekontorOrgnr" to "",
+                "avsenderFnr" to "08201023912",
+                "pasientIdent" to pasientFnr,
+                "behandlerFnr" to pasientFnr,
+            ),
+        )
+
+        assertEquals(
+            result.treeResult.getOutcome(),
+            ValideringRule.Outcomes.BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR,
+        )
+    }
+
+    @Test
     fun `Ugyldig orgnummer lengde, Status INVALID`() {
         val person31Years = LocalDate.now().minusYears(31)
         val pasientFnr = generatePersonNumber(person31Years, false)
@@ -229,59 +351,6 @@ class ValideringRulesTest {
         assertEquals(
             result.treeResult.getOutcome(),
             ValideringRule.Outcomes.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR,
-        )
-    }
-
-    @Test
-    fun `Behandler samme som pasient, Status INVALID`() {
-        val pasientFnr = generatePersonNumber(LocalDate.now().minusYears(31), false)
-
-        val result =
-            ValideringRules(
-                    ValideringRulePayload(
-                        sykmeldingId = "sykmeldingId",
-                        rulesetVersion = "2",
-                        aktivitet = emptyList(),
-                        legekontorOrgnr = null,
-                        behandlerFnr = pasientFnr,
-                        pasientIdent = pasientFnr,
-                        avsenderFnr = "08201023912",
-                        utdypendeOpplysninger = emptyMap(),
-                        papirsykmelding = false,
-                    )
-                )
-                .execute(ExecutionMode.NORMAL)
-
-        assertEquals(result.treeResult.status, RuleStatus.INVALID)
-        assertPath(
-            result.rulePath,
-            listOf(
-                ValideringRule.UGYLDIG_ORGNR_LENGDE to false,
-                ValideringRule.PAPIRSYKMELDING to false,
-                ValideringRule.UGYLDIG_REGELSETTVERSJON to false,
-                ValideringRule.MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39 to false,
-                ValideringRule.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR to false,
-                ValideringRule.BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR to true,
-            ),
-        )
-
-        assertEquals(
-            result.ruleInputs,
-            mapOf(
-                "rulesetVersion" to "2",
-                "papirsykmelding" to false,
-                "sykmeldingPerioder" to emptyList<Any>(),
-                "utdypendeOpplysninger" to emptyMap<String, Any>(),
-                "legekontorOrgnr" to "",
-                "avsenderFnr" to "08201023912",
-                "pasientIdent" to pasientFnr,
-                "behandlerFnr" to pasientFnr,
-            ),
-        )
-
-        assertEquals(
-            result.treeResult.getOutcome(),
-            ValideringRule.Outcomes.BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR,
         )
     }
 }
