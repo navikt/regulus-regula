190172ec17f6289183678cf692f5573d5093bf5e
Karl J. Overå <k@rl.run>
2025-09-22 16:05:18 +0200

    fix: messageForSender with weird package name

--- lib/src/main/kotlin/no/nav/tsm/regulus/regula/rules/trees/arbeidsuforhet/ArbeidsuforhetRuleDefinitions.kt
+++ lib/src/main/kotlin/no/nav/tsm/regulus/regula/rules/trees/arbeidsuforhet/ArbeidsuforhetRuleDefinitions.kt
@@ -26,7 +26,7 @@ internal enum class ArbeidsuforhetRule {
             messageForSender =
                 "Sykmeldingen kan ikke rettes, det må skrives en ny. " +
                     "Pasienten har fått beskjed om å vente på ny sykmelding fra deg. Grunnet følgende:" +
-                    "no.nav.tsm.regulus.regula.payload.Hoveddiagnose eller annen lovfestet fraværsgrunn mangler. ",
+                    "Hoveddiagnose eller annen lovfestet fraværsgrunn mangler. ",
             messageForUser = "Den må ha en hoveddiagnose eller en annen gyldig fraværsgrunn.",
         ),
         UGYLDIG_KODEVERK_FOR_HOVEDDIAGNOSE(
